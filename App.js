3165fb639f52b51ac0fd7fb491fe99b83cb23ce5
PhungCanhTuan <tuancanh252@gmail.com>
2024-09-09 11:40:05 +0700

    Thuc hanh tuan 4

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Screen_1 from "./screens/Screen_1";
+import Screen_2 from "./screens/Screen_2";
+import Screen_3 from "./screens/Screen_3";
+import Screen_4 from "./screens/Screen_4";
+
+export default function App() {
+  return (
+    //<Screen_1 />
+    //<Screen_2/>
+    //<Screen_3 />
+    <Screen_4 />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
